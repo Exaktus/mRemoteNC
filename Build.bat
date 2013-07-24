1a072e2efc324e49e66f9f67144dffa7f34979c9
Exaktus <mRemoteNC@ya.ru>
2013-07-24 12:07:44 +0300

    Improve reliability of Build.bat

--- Build.bat
+++ Build.bat
@@ -3,12 +3,26 @@
 set RAR="C:\Program Files\WinRAR\WinRAR.exe"
 set VCVARSALL="%ProgramFiles(x86)%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 set DEVENV="devenv.exe"
-set ISS="%ProgramFiles(x86)%\Inno Setup 5\iscc.exe"
+set ISS="%ProgramFiles(x86)%\Innoj Setup 5\iscc.exe"
 
 call %VCVARSALL% x86
 
 rmdir /s /q "%~dp0\mRemoteV2\bin" > nul 2>&1
 rmdir /s /q "%~dp0\mRemoteV2\obj" > nul 2>&1
+
+if exist "%~dp0\mRemoteV1\bin" goto ERROR_RMDIR
+if exist "%~dp0\mRemoteV1\obj" goto ERROR_RMDIR
+goto NOERROR_RMDIR
+
+:ERROR_RMDIR
+echo.
+echo Could not clean output directories.
+echo.
+echo Build process failed.
+echo.
+goto END
+
+:NOERROR_RMDIR
 
 echo Building release version...
 %DEVENV% "%~dp0\mRemoteV2.sln" /build "Release"
@@ -18,6 +32,19 @@ echo Building portable version...
 
 mkdir "%~dp0\Release" > nul 2>&1
 del /f /q "%~dp0\Release\*.*" > nul 2>&1
+
+if not exist %ISS% goto ERROR_ISS
+goto NOERROR_ISS
+
+:ERROR_ISS
+echo.
+echo Inno Setup not found.
+echo.
+echo Build process failed.
+echo.
+goto END
+
+:NOERROR_ISS
 
 %ISS% "%~dp0\Installer\mRemoteNC.iss"
 
@@ -51,5 +78,9 @@ copy "%~dp0\Release\%AioName%" "%~dp0\Release\mRemoteNC-AIO-Latest.zip"
 copy "%~dp0\Release\%PortableZipName%" "%~dp0\Release\mRemoteNC-Portable-Latest.zip"
 copy "%~dp0\Release\%InstallerReleaseName%" "%~dp0\Release\mRemoteNC-Installer-Latest.exe"
 
+echo.
 echo Finished!
+echo.
+
+:END
 pause
